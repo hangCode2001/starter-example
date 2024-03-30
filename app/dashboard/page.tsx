37ec14a3959184971c944f340bbe54e17d2c33b6
魏航宇 <weihangyu@bytedance.com>
2024-03-31 01:14:13 +0800

    feat: add数据

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,39 @@
-export default function Page() {
-    return <p>Dashboard Page</p>;
-  }
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import { fetchRevenue,fetchLatestInvoices,fetchCardData } from '@/app/lib/data';
+ 
+export default async function Page() {
+    const revenue = await fetchRevenue();
+    const latestInvoices = await fetchLatestInvoices();
+    const {
+        numberOfInvoices,
+        numberOfCustomers,
+        totalPaidInvoices,
+        totalPendingInvoices,
+      } = await fetchCardData();
+
+    console.log("%c Line:9 🥤 revenue", "color:#7f2b82", revenue);
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        仪表板
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="已收款" value={totalPaidInvoices} type="collected" />
+        <Card title="待处理" value={totalPendingInvoices} type="pending" />
+        <Card title="总发票数" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="总客户数"
+          value={numberOfCustomers}
+          type="customers"
+        />
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue}  />
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
+    </main>
+  );
+}
